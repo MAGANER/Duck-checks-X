3bbcf3fb77c624f60789c3efcf42a71d6c34b0a4
MAGANER <maganer.krik@mail.ru>
2024-01-19 00:29:48 +0300

    dumb hack to check whether passed argument is dir

--- listdir.h
+++ listdir.h
@@ -2,19 +2,24 @@
 #define LISTDIR_H
 #include <stdio.h>
 #include <sys/types.h>
+#include<sys/stat.h>
 #include <dirent.h>
 #include<string>
 #include<vector>
 
 typedef std::vector<std::string> svec;
 
+static bool is_dir(const std::string& name)
+{
+  return name == "word" || name == "_rels" || name == "docProps" || name == "customXml";
+}
+
+
 /*
   based on https://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html
 */
-extern svec get_high_level_content(const std::string& docx_dir)
+extern void get_high_level_content(const std::string& docx_dir, svec& files, svec& dirs)
 {
-  svec content;
-
   DIR *dp;
   struct dirent *ep;
 
@@ -25,7 +30,12 @@ extern svec get_high_level_content(const std::string& docx_dir)
       {
 	std::string dir  = std::string(ep->d_name);
 	if(dir != "." && dir != "..")
-	  content.push_back(std::string(ep->d_name));
+	  {
+	    if(is_dir(dir))
+	      dirs.push_back(std::string(ep->d_name));
+	    else
+	      files.push_back(std::string(ep->d_name));
+	  }
       }
       (void) closedir (dp);
     }
@@ -33,8 +43,6 @@ extern svec get_high_level_content(const std::string& docx_dir)
     {
       printf("failed to open %s directory!\n",docx_dir.c_str());
     }
-    
-  return content;
 }
 
 
