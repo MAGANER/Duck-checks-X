a10706819d29e1adfcbde9a35c26473a08d45605
MAGANER <maganer.krik@mail.ru>
2024-01-18 19:37:09 +0300

    added structure with results of all computations

---
+++ compare.h
@@ -0,0 +1,28 @@
+#ifndef COMPARE_H
+#define COMPARE_H
+#include"strcomp.h"
+#include"listdir.h"
+#include<string>
+
+struct Comparision
+{
+  double high_level;
+
+  Comparision(double high_level):high_level(high_level){}
+  ~Comparision(){}
+};
+
+static double compare_on_high_level(const std::string& file1, const std::string& file2)
+{
+  auto content1 = get_high_level_content(file1+".ext");
+  auto content2 = get_high_level_content(file2+".ext");
+  printf("%s\n %s\n",content1.c_str(),content2.c_str());
+  return CSDP(content1,content2);
+}
+
+extern Comparision* compare(const std::string& file1, const std::string& file2)
+{
+  auto high_level_diff = compare_on_high_level(file1,file2);
+  return new Comparision(high_level_diff);
+}
+#endif
