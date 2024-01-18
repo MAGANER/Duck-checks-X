1d48dbab82789e862db768910511404d4fa859b2
MAGANER <maganer.krik@mail.ru>
2024-01-18 14:08:13 +0300

    header checks provided file is docx

---
+++ mbytesreader.h
@@ -0,0 +1,35 @@
+#ifndef MBYTESREADER_H
+#define MBYTESREADER_H
+#include<string>
+#include<stdio.h>
+
+static int read_file_magic_bytes(const char const* f)
+{
+	FILE* fp = fopen(f, "rb");
+	if (fp == NULL)
+	{
+		printf("DCX error: unable to read %s's magic bytes!\n", f);
+		return -1;
+	}
+	unsigned char magic[4];
+	if (fread((void*)magic, 1, 4, fp) != 4)
+	{
+		printf("DCX error: failed to read %s's magic bytes!\n", f);
+		fclose(fp);
+		return -1;
+	}
+	fclose(fp);
+
+
+	int summ = 0;
+	for (size_t i = 0; i < 4; ++i)summ += int(magic[i]);
+	return summ;
+}
+
+extern bool is_docx_file(const char const* f)
+{
+	return read_file_magic_bytes(f) == 162;
+}
+
+
+#endif
