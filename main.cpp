173b7258192fcae6a3ee753c7beb78e0cff80c4e
MAGANER <maganer.krik@mail.ru>
2024-01-18 15:06:02 +0300

    gets arguments, check their correctness

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include"strcomp.h"
+#include"mbytesreader.h"
+#include<stdio.h>
+
+int prepare(int argc, char** argv);
+int main(int argc, char** argv)
+{
+	if (prepare(argc, argv) == -1) return -1;
+
+
+	return 0;
+}
+int prepare(int argc, char** argv)
+{
+	if (argc != 3)
+	{
+		printf("DCX error: incorrect number of arguments! \n Usage example: dcx file1.docx file2.docx\n");
+		return -1;
+	}
+
+	if (!is_docx_file(argv[1]))
+	{
+		printf("DCX error: %s must be a docx file!\n", argv[1]);
+		return -1;
+	}
+	if (!is_docx_file(argv[2]))
+	{
+		printf("DCX error: %s must be a docx file!\n", argv[2]);
+		return -1;
+	}
+
+	return 0;
+}
