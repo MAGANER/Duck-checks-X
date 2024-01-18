169f763a9a072af754bef4e002e5ae049ed876b9
MAGANER <maganer.krik@mail.ru>
2024-01-18 18:45:00 +0300

    extract docx

--- main.cpp
+++ main.cpp
@@ -6,19 +6,22 @@
 #define TS(x) std::string(x)
 
 inline int prepare(int argc, char** argv);
+inline void extract_docx(const std::string& file1);
 int main(int argc, char** argv)
 {
   //1) check input data correctness
 	if (prepare(argc, argv) == -1) return -1;
- 
+  //2) extract each docx to its own specific dir
+	extract_docx(TS(argv[1]));
+	extract_docx(TS(argv[2]));
        
 	return 0;
 }
 int prepare(int argc, char** argv)
 {
-	if (argc != 4)
+	if (argc != 3)
 	{
-		printf("DCX error: incorrect number of arguments! \n Usage example: dcx file1.docx file2.docx convertioapi_key\n");
+		printf("DCX error: incorrect number of arguments! \n Usage example: dcx file1.docx file2.docx\n");
 		return -1;
 	}
 
@@ -34,4 +37,11 @@ int prepare(int argc, char** argv)
 	}
 
 	return 0;
+}
+void extract_docx(const std::string& file)
+{
+  auto command  = "mkdir "+file+".ext && ";
+  command      += "cd "+file+".ext && ";
+  command      += "unzip ../"+file;
+  system(command.c_str());
 }
